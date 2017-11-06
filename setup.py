bf069ad615a2f9091f50a468b16a75a11ca9cc09
Blake Nedved <blake@Blakes-MacBook-Pro.local>
2017-11-06 13:30:07 -0600

    Updated setup.py to a higher version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyscribble',
-    version='0.1',
+    version='0.1.1',
     description='A scribbletune-like library for Python3',
     url='https://github.com/blakeanedved/pyscribble',
     author='Blake Nedved',
